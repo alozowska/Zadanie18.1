1adb0d1c911763d7811001e6c7a1c0374e593f1d
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-09-22 15:54:56 +0200

    ver 1.0

--- src/main/java/pl/javastart/springboot/controller/ProductController.java
+++ src/main/java/pl/javastart/springboot/controller/ProductController.java
@@ -1,22 +1,26 @@
 package pl.javastart.springboot.controller;
 
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import pl.javastart.springboot.entity.CategoryProduct;
 import pl.javastart.springboot.entity.InfoProducts;
 import pl.javastart.springboot.entity.Product;
 import pl.javastart.springboot.repository.ProductRepository;
 
 
-
-@RestController
+@Controller
 public class ProductController {
 
     private final ProductRepository productRepository;
@@ -26,28 +30,67 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    @GetMapping(value = "/lista")
-    public InfoProducts getAllProductsFromCategory(@RequestParam(value = "kategoria", required = false)String category) {
-        if (category != null) {
+/*    @RequestMapping(value = {"/","","/index","czydziala"})
+    public String getIndexPage(){
+        System.out.println("test");
+        return "index.html";
+    }*/
 
-            return productRepository.findByCategory(category);
-        } else
-            return productRepository.allProducts();
+    @RequestMapping(value = "/lista", method = RequestMethod.GET)
+    @ResponseBody
+    public String getAllProductsFromCategory(@RequestParam(value = "kategoria", required = false) String category) {
+        StringBuilder stringBuilder = new StringBuilder();
+        InfoProducts products;
+        double sum = 0;
+        if (category != null) {
+            products = productRepository.findByCategory(category);
+        } else {
+            products = productRepository.allProducts();
+        }
+        for (Product product : products.getProducts()) {
+            sum += product.getPrice();
+            stringBuilder.append(product.getName()).append("<br>");
+        }
+        stringBuilder.append("<br>").append(sum).append(" zł");
+        return stringBuilder.toString();
     }
 
-    @RequestMapping(value = "/add")
-    public String addProduct(@RequestParam String name,
-                             @RequestParam double price,
-                             @RequestParam CategoryProduct categoryProduct) {
 
-      //  Product product = new Product(name, price, categoryProduct);
+    @GetMapping(value = {"/add"})
+    public String addProductForm() {
         return "dodawanie.html";
     }
 
-        @PostMapping("/add")
 
-    public Product saveProduct(@RequestBody Product product) {
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addProduct(@RequestParam("name") String name,
+                             @RequestParam("price") Double price,
+                             @RequestParam("category") String category) {
+        productRepository.save(new Product(name, price, CategoryProduct.valueOf(category)));
+        return "redirect:/";
 
-        return productRepository.save(product);
-   }
+//        Product product = new Product(name, price, CategoryProduct.valueOf(category));
+
+
+//        product.setName(name);
+//        product.setPrice(price);
+
+/*        switch (category) {
+            case "SPOZYWCZE":
+                product.setCategoryProduct(CategoryProduct.SPOZYWCZE);
+                break;
+            case "DOMOWE":
+                product.setCategoryProduct(CategoryProduct.DOMOWE);
+                break;
+            case "INNE":
+                product.setCategoryProduct(CategoryProduct.INNE);
+                break;
+            default:
+                product.setCategoryProduct(null);
+                break;
+        }*/
+
+//        product.setCategoryProduct(CategoryProduct.valueOf(category));
+
+    }
 }
--- src/main/java/pl/javastart/springboot/entity/CategoryProduct.java
+++ src/main/java/pl/javastart/springboot/entity/CategoryProduct.java
@@ -5,4 +5,5 @@ public enum CategoryProduct {
     SPOZYWCZE, //
     DOMOWE, //
     INNE //
+
 }
